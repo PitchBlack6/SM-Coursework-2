b717134877eba33374a66ca7de01ed09548ab724
jonathanooikw <45030711+jonathanooikw@users.noreply.github.com>
2018-12-06 20:19:37 +0800

    Changed so item sprites showed up now

--- DiamondHunter/src/com/neet/MapViewer/Controller.java
+++ DiamondHunter/src/com/neet/MapViewer/Controller.java
@@ -1,28 +1,34 @@
 package com.neet.MapViewer;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
- 
-import com.neet.MapViewer.ModelMap;
-
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 public class Controller {
-	
 	
 	@FXML private Button something;
 	@FXML private Canvas MapCanvas;
 
     @FXML private Text actiontarget;
     
+    @FXML private ImageView item; //new image view
+    private Image axeImage;
+    private Image boatImage;
+    
     @FXML protected void setAxe(ActionEvent event) {
         actiontarget.setText("Click to set Axe position.");
+        drawItem("axe",20,20); //test draws axe to the middle of the map
+        						// just need to change the 20s to the mouse coordinates
     }
     @FXML protected void setBoat(ActionEvent event) {
         actiontarget.setText("Click to set Boat position.");
+        drawItem("boat",20,20); // 20 is in tiles
     }
 	
 	private GraphicsContext graphics;
@@ -31,6 +37,8 @@ public class Controller {
 	
 	public Controller() {
 		modelMap = new ModelMap();
+		axeImage = SwingFXUtils.toFXImage(Content.ITEMS[1][1],null); // assign converted buffered image to axeimage
+		boatImage = SwingFXUtils.toFXImage(Content.ITEMS[1][0],null); //assign converted boat buffered image to boat image
 	}
 	
 	public void initialize() {
@@ -39,4 +47,14 @@ public class Controller {
 
 	}
 	
+	private void drawItem(String item, int x, int y) { //draw items
+		if (item.equals("axe"))	
+		{
+			graphics.drawImage(axeImage, x*16, y*16);//*16 to convert to pixels
+		}
+		else 
+			{
+			graphics.drawImage(boatImage, x*16, y*16);
+			}
+	}
 }
