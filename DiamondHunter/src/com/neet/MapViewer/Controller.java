0490fd208805b14af0eec3365edfb01e42e3c31d
faraeliana98 <44489003+faraeliana98@users.noreply.github.com>
2018-12-12 17:21:09 +0800

    Improved the GUI of game
    Changed the layout of the game and included a Start button to direct user to the game.

--- DiamondHunter/src/com/neet/MapViewer/Controller.java
+++ DiamondHunter/src/com/neet/MapViewer/Controller.java
@@ -12,6 +12,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import com.neet.DiamondHunter.Main.Game;
+
 public class Controller {
 	
 	@FXML private Button something;
@@ -24,14 +26,16 @@ public class Controller {
     public int mousex;
     public int mousey;
     public int itemID = 0; //creates flag to track current item ID
-  
+    
+    private GraphicsContext graphics;
 	private ModelMap modelMap;
 	
 	
 	public Controller() {
 		modelMap = new ModelMap();
 	}
-  
+    
+	
 	public void initialize() {
 		graphics = MapCanvas.getGraphicsContext2D();
 		modelMap.draw(graphics);
@@ -39,16 +43,53 @@ public class Controller {
 	}
     
     @FXML protected void setAxe(ActionEvent event) {
-        actiontarget.setText("Click to set Axe position.");
+        actiontarget.setText("Click to set Axe position");
         itemID = 1;
           
     }
     @FXML protected void setBoat(ActionEvent event) {
-        actiontarget.setText("Click to set Boat position.");
+        actiontarget.setText("Click to set Boat position");
         itemID = 2; //sets itemID to ID for boat
+    }
+    
+    @FXML protected void saveLocation(ActionEvent event) {
+    	try {
+    	File file = new File("Location.txt");
+    	if(!file.exists()) {
+				file.createNewFile();
+			}
+    	
+    	PrintWriter pw = new PrintWriter(file);
+    	pw.print(modelMap.getAxeRow() + " " + modelMap.getAxeCol() + "\n" + modelMap.getBoatRow() + " " + modelMap.getBoatCol());
+    	pw.close();
+    	
+    	}
+    	
+    	catch(IOException e){
+    		e.printStackTrace();
+    	}
+    		
+    	
+        actiontarget.setText("Location saved");
+          
+    }
+    
+    @FXML protected void setDefault(ActionEvent event) {
+    	actiontarget.setText("Position set to default");
+    	modelMap.setItemLocation(1, 26, 37, graphics);
+    	modelMap.setItemLocation(2, 12, 4, graphics);
+    }
+    
+    @FXML protected void startGame(ActionEvent event) {
+    	MapMain.primaryStage.hide();
+    	Game.main(null);
+    }
+    
     
     @FXML protected void mousePosition(MouseEvent event) {
         	int col = (int) (event.getSceneX()/16); 
         	int row = (int) (event.getSceneY()/16);
+        	String message = modelMap.setItemLocation(itemID, row, col, graphics);
+        	actiontarget.setText(message);
     }
 }
